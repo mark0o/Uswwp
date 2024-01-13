0d44b780c93a7e5d9ed65ff35d6a6dec59c9bf92
Bazztube <80954930+baztube@users.noreply.github.com>
2024-01-13 19:51:48 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,13 +1,16 @@
 const userAgent = navigator.userAgent;
 const cover = document.getElementById("cover")
+const body = document.querySelector("body")
 
 if (userAgent.match(/Android/i)) {
     console.log("Device is a phone or tablet");
     cover.style.visibility = "visible"
+    body.style.overflow = "hidden"
 
 } else if (userAgent.match(/iPad|iPhone|iPod/i)) {
     console.log("Device is an iPhone or iPad");
     cover.style.visibility = "visible"
+    body.style.overflow = "hidden"
 } else if (userAgent.match(/Windows|Mac|Linux/i)) {
     console.log("Device is a computer");
     cover.style.visibility = "hidden"
