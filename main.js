8534dbabcb86549ab628dfa135824bf60614d566
Marko <80954930+mark0o@users.noreply.github.com>
2024-10-19 17:51:49 +0200

    Update main.js

--- main.js
+++ main.js
@@ -5,17 +5,17 @@ const body = document.querySelector("body")
 if (userAgent.match(/Android/i)) {
     console.log("Device is a phone or tablet");
     cover.style.visibility = "visible"
-    body.style.overflow = "hidden"
+    body.style.display = "none"
 
 } else if (userAgent.match(/iPad|iPhone|iPod/i)) {
     console.log("Device is an iPhone or iPad");
     cover.style.visibility = "visible"
-    body.style.overflow = "hidden"
+    body.style.display = "none"
 } else if (userAgent.match(/Windows|Mac|Linux/i)) {
     console.log("Device is a computer");
-    cover.style.visibility = "hidden"
+    cover.style.visibility = "none"
 } else {
     console.log("Device type is unknown or not detected");
     cover.style.visibility = "visible"
-    body.style.overflow = "hidden"
+    body.style.display = "none"
 }
