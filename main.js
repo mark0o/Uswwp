8a496c9e372d02363d9772abfd9d5502302c5679
Marko <80954930+mark0o@users.noreply.github.com>
2024-10-19 17:54:37 +0200

    Update main.js

--- main.js
+++ main.js
@@ -4,18 +4,18 @@ const body = document.querySelector("body")
 
 if (userAgent.match(/Android/i)) {
     console.log("Device is a phone or tablet");
-    cover.style.visibility = "visible"
+    cover.style.display = "block"
     body.style.display = "none"
 
 } else if (userAgent.match(/iPad|iPhone|iPod/i)) {
     console.log("Device is an iPhone or iPad");
-    cover.style.visibility = "visible"
+    cover.style.display = "block"
     body.style.display = "none"
 } else if (userAgent.match(/Windows|Mac|Linux/i)) {
     console.log("Device is a computer");
-    cover.style.visibility = "none"
+    cover.style.display = "none"
 } else {
     console.log("Device type is unknown or not detected");
-    cover.style.visibility = "visible"
+    cover.style.display = "block"
     body.style.display = "none"
 }
